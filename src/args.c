fc63dd4755c76a2b286cf905b64e0831c34ddfae
vladislav <yrik.bezrukov.45@mail.ru>
2026-09-04 13:16:43 +0700

    Заполнены файлы argh.h и argh.c

--- src/args.c
+++ src/args.c
@@ -1,2 +1,19 @@
+#include "../include/args.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+int
+CheckPID (char *CharPID)
+{
+    if (CharPID == NULL)
+        return -1;
+    char* EndChar = NULL;
+    int IntPID = strtol(CharPID, &EndChar, 10);
+    
+    if(IntPID < 0 || (*EndChar != (NULL || "\n"))){
+        return -1;
+    }
+    else{
+        return IntPID;
+    }
+}
